6e8b36653bd1c55f9271590b8a58063504c64b92
isakov <isakov4852@mail.ru>
2025-05-28 20:13:20 +0300

    commit a

--- src/components/UI/input/MyInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './input.module.css';
-
-const MyInput = React.forwardRef((props, ref) => {
-    return (      
-        <input ref={ref} className={classes.myInput} {...props}/>     
-    )
-})
-export default MyInput;
